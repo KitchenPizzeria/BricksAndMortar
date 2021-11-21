0c4f5511306f80eab67b27fa4d0bd73c5afa9384
KitchenPizzeria <valomeyrick2012@hotmail.co.uk>
2021-11-21 21:59:28 +0000

    update

---
+++ terraform/main.tf
@@ -0,0 +1,72 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = var.region
+}
+resource "aws_instance" "ec2_example" {
+
+  ami                    = "ami-0767046d1677be5a0"
+  instance_type          = "t2.micro"
+  key_name               = "aws_key"
+  vpc_security_group_ids = [aws_security_group.main.id]
+
+  provisioner "remote-exec" {
+    inline = [
+      "touch hello.txt",
+      "echo helloworld remote provisioner >> hello.txt",
+    ]
+  }
+  connection {
+    type        = "ssh"
+    host        = self.public_ip
+    user        = "ubuntu"
+    private_key = file("/home/rahul/Jhooq/keys/aws/aws_key")
+    timeout     = "4m"
+  }
+}
+
+resource "aws_security_group" "main" {
+  egress = [
+    {
+      cidr_blocks      = ["0.0.0.0/0", ]
+      description      = ""
+      from_port        = 0
+      ipv6_cidr_blocks = []
+      prefix_list_ids  = []
+      protocol         = "-1"
+      security_groups  = []
+      self             = false
+      to_port          = 0
+    }
+  ]
+  ingress = [
+    {
+      cidr_blocks      = ["0.0.0.0/0", ]
+      description      = ""
+      from_port        = 22
+      ipv6_cidr_blocks = []
+      prefix_list_ids  = []
+      protocol         = "tcp"
+      security_groups  = []
+      self             = false
+      to_port          = 22
+    }
+  ]
+}
+
+
+resource "aws_key_pair" "deployer" {
+  key_name   = "aws_key"
+  public_key = ""
+}
+
+output "serverip" {
+  value = aws_instance.app.public_ip
+}
---
+++ terraform/variables.tf
@@ -0,0 +1,16 @@
+variable "AWS_ACCESS_KEY" {
+  default     = "ASIAYZFWRJC4TCGWFR4N"
+  description = "This is the Admin AWS_ACCESS_KEY"
+  type        = string
+}
+
+variable "AWS_SECRET_ACCESS_KEY" {
+  default     = "+4tCoR+kbEwB0XkYgDhEMtqMTuQ2uI0JDsc1MiEm"
+  description = "This is the Admin AWS_SECRET_ACCESS_KEY"
+  type        = string
+}
+
+variable "region" {
+  type    = string
+  default = "eu-west-1"
+}
